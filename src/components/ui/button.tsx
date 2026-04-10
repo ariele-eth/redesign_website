6c05925e9825b054fc6ff89b924287e2fc26f044
Ariele <ariele.marcellino2005@gmail.com>
2026-04-10 16:27:54 +0200

    change button hover color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        default: "bg-primary text-primary-foreground hover:bg-[#2eb9ff]",
+        destructive: "bg-destructive text-destructive-foreground hover:bg-[#2eb9ff]",
+        outline: "border border-input bg-background hover:bg-[#2eb9ff]",
+        secondary: "bg-secondary text-secondary-foreground hover:bg-[#2eb9ff]",
+        ghost: "hover:bg-[#2eb9ff]",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
